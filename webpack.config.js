d73bfff4ed65331aff4bb877d95e1bfe15603126
EL MOSSAOUI Redouan <redouan.el-mossaoui@cegedim.com>
2019-10-15 18:17:30 +0100

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path')
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+    mode   : 'development',
+    entry  : './src/index.js',
+    output : {
+        path    : path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+
+    module: {
+        rules: [
+            { 
+                test: /\.js$/, 
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-react']
+                    }
+                } 
+            }
+        ]
+    },
+
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        compress: true,
+        port: 9000
+    },
+
+    plugins: [
+        new CopyWebpackPlugin([{ from: './*.html' }])
+    ]
+}
